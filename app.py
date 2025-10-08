02c33bc0747771b871788a27666c1f26876d284e
MH <minvuor@gmail.com>
2025-10-08 14:52:08 +0300

    Refactor registration and login

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import sqlite3
 from flask import Flask
 from flask import abort, redirect, render_template, request, session
-from werkzeug.security import check_password_hash, generate_password_hash
 import config
 import db
 import looks
@@ -135,20 +134,18 @@ def register() :
 #Create account
 @app.route("/create", methods=["POST"])
 def create() :
-    username = request.form["username"]
-    password1 = request.form["password1"]
-    password2 = request.form["password2"]
-    if password1 != password2:
-        return "VIRHE: salasanat eivät ole samat"
-    password_hash = generate_password_hash(password1)
+	username = request.form["username"]
+	password1 = request.form["password1"]
+	password2 = request.form["password2"]
+	if password1 != password2:
+		return "VIRHE: salasanat eivät ole samat"
 
-    try:
-        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
-        db.execute(sql, [username, password_hash])
-    except sqlite3.IntegrityError:
-        return "VIRHE: tunnus on jo varattu"
+	try :
+		users.create_user(username, password1)
+	except sqlite3.IntegrityError:
+		return "VIRHE: tunnus on jo varattu"
 
-    return "Tunnus luotu"
+	return "Tunnus luotu"
 
 #Login
 @app.route("/login", methods=["GET", "POST"])
@@ -160,12 +157,8 @@ def login() :
 		username = request.form["username"]
 		password = request.form["password"]
 
-		sql = "SELECT id, password_hash FROM users WHERE username = ?"
-		result = db.query(sql, [username])[0]
-		user_id = result["id"]
-		password_hash = result["password_hash"]
-
-		if check_password_hash(password_hash, password):
+		user_id = users.check_login(username, password)
+		if user_id :
 			session["user_id"] = user_id
 			session["username"] = username
 			return redirect("/")
